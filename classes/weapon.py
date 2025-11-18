4e1693d5a8d0055113d8f9ef46c4497f2bb72ed9
Honapona_ <156892939+Honapon@users.noreply.github.com>
2025-11-18 20:40:08 +0100

    jeg classes og metoder wallah

---
+++ classes/weapon.py
@@ -0,0 +1,10 @@
+
+
+class Weapon:
+    def __init__(self, typ:str, dmg:int):
+        self._typ = typ
+        self._dmg = dmg
+
+    def __str__(self):
+        return f"Your Weapon is a {self._typ}, dealing {self._dmg} dmg"
+        
