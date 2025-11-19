6f545d496f8b3f61c6b0d3a5f907757f151a7704
Honapona_ <156892939+Honapon@users.noreply.github.com>
2025-11-19 09:52:56 +0100

    i push

--- classes/enemy.py
+++ classes/enemy.py
@@ -2,5 +2,9 @@ from .entity import Entity
 from .weapon import Weapon
 
 class Enemy(Entity, Weapon):
-    def __init__(self, name: str, hp: int, defence: int, strength: float):
+    def __init__(self, name: str, hp: int, defence: float, strength: float, typ:str, dmg:int):
         Entity.__init__(self, name, hp, defence, strength)
+        Weapon.__init__(self, typ, dmg)
+        
+    def __str__(self):
+        return super().__str__()
--- classes/player.py
+++ classes/player.py
@@ -1,23 +1,21 @@
-from .entity import Entity
-from .weapon import Weapon
+from entity import Entity
+from weapon import Weapon
 from random import randint
 
+
 class Player(Entity, Weapon):
-    def __init__(self, name: str, hp: int, defence: int, strength: float, typ: str, dmg: int ):
+    def __init__(self, name: str, hp: int, defence: float, strength: float, typ: str, dmg: int ):
         Entity.__init__(self, name, hp, defence, strength)
-        self._hp = hp
-        self._defence = defence
-        self._strength = strength
         Weapon.__init__(self, typ, dmg)
         self._dmg = 0
         if typ == "Pinne":
-            self._dmg = 5
+            self._dmg = 2
         if typ == "Katana":
-            self._dmg = 38
+            self._dmg = 8
         elif typ == "Polearm":
-            self._dmg = 40
+            self._dmg = 10
         elif typ == "bow":
-            self._dmg = 28
+            self._dmg = 7
         
     def __str__(self):
         return Entity.__str__(self) + Weapon.__str__(self)
@@ -27,22 +25,20 @@ class Player(Entity, Weapon):
         critRate = randint(1,2)
         atk = self._dmg / self._strength
         critDmg = atk * critRate 
-        print(f"You Did {critDmg} dmg to your enemy")
-        return critDmg 
-
+        print(f"You did {round(critDmg) * self._defence} damage to fiend")
+        return round(critDmg)
     def dmg_taken(self):
         self._hp -= Player.attack(self) * self._defence 
-        
         if self._hp > 0:
             print(f"you have {self._hp} hp remaining")
         elif self._hp < 0:
             self._hp = 0
             print("You have died")
 
-# spiller = Player("bob", 150, 20, 50, "Polearm", 45)
+spiller = Player("bob", 150, 1.5, 0.7, "Polearm", 45)
 # print(spiller)
 # Player.attack(spiller)
-# Player.dmg_taken(spiller)
+Player.dmg_taken(spiller)
 
 
         
