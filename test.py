5c27a5b6a91c496f4f67c88ba268625682ed21e3
Honapona_ <156892939+Honapon@users.noreply.github.com>
2025-11-30 21:07:54 +0100

    a

---
+++ test.py
@@ -0,0 +1,12 @@
+import time
+import os
+from random import randint
+from classes import Player, Enemy
+import pytest
+
+
+def test_heal():
+    p = Player("test",100,1,0.7,"Pinne",0)
+    p.dmg_verden(20)
+    assert p.liv_igjenn()
+    
